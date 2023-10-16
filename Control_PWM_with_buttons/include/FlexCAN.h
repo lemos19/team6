1b38be15799446b347a4613238af912ee30dfcb6
lemons <chanhvuive@gmail.com>
2023-10-16 10:44:20 +0700

    last change

--- Control_PWM_with_buttons/include/FlexCAN.h
+++ Control_PWM_with_buttons/include/FlexCAN.h
@@ -4,49 +4,41 @@
 #define FLEXCAN0_BASE_ADDRESS 0x40024000
 #define FLEXCAN1_BASE_ADDRESS 0x40025000
 #define FLEXCAN2_BASE_ADDRESS 0x4002B000
-#define CAN_MCR_MDIS_MASK  0x80000000u
+#define CAN_MCR_MDIS_MASK 0x80000000u
 #define CAN_CTRL1_CLKSRC_MASK 0x2000u
-#define CAN_MCR_FRZACK_MASK   0x1000000u
-#define CAN_MCR_FRZACK_SHIFT  24u
-typedef union 
+#define CAN_MCR_FRZACK_MASK 0x1000000u
+#define CAN_MCR_FRZACK_SHIFT 24u
+#define CAN_MCR_NOTRDY_MASK 0x8000000u
+#define CAN_MCR_NOTRDY_SHIFT 27u
+typedef union
 {
     uint32_t Register;
-    struct 
+    struct
     {
-        
-
-
-
-
-
-
-
-        
-        uint32_t RJW;
-        uint32_t PSEG1;
-        uint32_t PSEG2;
-        uint32_t BOFFMSK;
-        uint32_t ERRMSK;
-        uint32_t CLKSRC;
-        uint32_t LPB;
-        uint32_t TWRNMSK;
-        uint32_t RWRNMSK;
-        uint32_t SMP;
-        uint32_t BOFFREC;
-        uint32_t TSYN;
-        uint32_t LBUF;
-        uint32_t LOM;
         uint32_t PROPSEG;
+        uint32_t LOM;
+        uint32_t LBUF;
+        uint32_t TSYN;
+        uint32_t BOFFREC;
+        uint32_t SMP;
+        uint32_t Reserved[2];
+        uint32_t RWRNMSK;
+        uint32_t TWRNMSK;
+        uint32_t LPB;
+        uint32_t CLKSRC;
+        uint32_t ERRMSK;
+        uint32_t BOFFMSK;
+        uint32_t PSEG2;
+        uint32_t PSEG1;
+        uint32_t RJW;
         uint32_t PRESDIV;
-    }Fields;
-    
+    } Fields;
+} CTRL1_t;
 
-};
-
-typedef struct 
+typedef struct
 {
     volatile uint32_t MCR;
-    volatile uint32_t CTRL1;
+    volatile CTRL1_t CTRL1;
     volatile uint32_t TIMER;
     uint32_t Reserved;
     volatile uint32_t RXMGMASK;
@@ -100,9 +92,9 @@ typedef struct
     volatile uint32_t FDCTRL;
     volatile uint32_t FDCBT;
     volatile uint32_t FDCRC;
-}FlexCAN_type;
+} FlexCAN_type;
 void FlexCAN0init(void);
-#define CAN0 ((FlexCAN_type*)(FLEXCAN0_BASE_ADDRESS))
-#define CAN1 ((FlexCAN_type*)(FLEXCAN1_BASE_ADDRESS))
-#define CAN2 ((FlexCAN_type*)(FLEXCAN2_BASE_ADDRESS))
+#define CAN0 ((FlexCAN_type *)(FLEXCAN0_BASE_ADDRESS))
+#define CAN1 ((FlexCAN_type *)(FLEXCAN1_BASE_ADDRESS))
+#define CAN2 ((FlexCAN_type *)(FLEXCAN2_BASE_ADDRESS))
 #endif
--- Control_PWM_with_buttons/src/FlexCAN.c
+++ Control_PWM_with_buttons/src/FlexCAN.c
@@ -6,10 +6,41 @@ void FlexCAN0init(void)
     uint32_t i;
     PCC->PCC_FlexCAN0 = 1u << CGC_SHIFT;
     CAN0->MCR |= CAN_MCR_MDIS_MASK;
-    CAN0->CTRL1 &= ~CAN_CTRL1_CLKSRC_MASK;
+    CAN0->CTRL1.Register &= ~CAN_CTRL1_CLKSRC_MASK;
     CAN0->MCR &= ~CAN_MCR_MDIS_MASK;
 
-    while (!((CAN0->MCR & CAN_MCR_FRZACK_MASK) >> CAN_MCR_FRZACK_SHIFT))  {}
+    while (!((CAN0->MCR & CAN_MCR_FRZACK_MASK) >> CAN_MCR_FRZACK_SHIFT))
+    {
+    }
+    CAN0->CTRL1.Register = 0;
+    CAN0->CTRL1.Fields.PSEG2 = 3;
+    CAN0->CTRL1.Fields.PSEG1 = 3;
+    CAN0->CTRL1.Fields.PROPSEG = 6;
+    CAN0->CTRL1.Fields.RJW = 3;
+    CAN0->CTRL1.Fields.SMP = 1;
+    for (i = 0; i < 128; i++)
+    {
+        CAN0->RAMn[i] = 0;
+    }
+    for (i = 0; i < 16; i++)
+    {
+        CAN0->RXIMRn[i] = 0xFFFFFFFF;
+    }
+    CAN0->RXMGMASK = 0x1FFFFFFF;
 
-    
+    CAN0->RAMn[4 * MSG_BUF_SIZE + 0] = 0x04000000; /* Msg Buf 4, word 0: Enable for reception 	*/
+                                                   /* EDL,BRS,ESI=0: CANFD not used 				*/
+                                                   /* CODE=4: MB set to RX inactive 				*/
+                                                   /* IDE=0: Standard ID 							*/
+                                                   /* SRR, RTR, TIME STAMP = 0: not applicable 	*/
+                                                   /* Node A receives msg with std ID 0x511 		*/
+    CAN0->RAMn[4 * MSG_BUF_SIZE + 1] = 0x14440000; /* Msg Buf 4, word 1: Standard ID = 0x111 		*/
+
+    CAN0->MCR = 0x0000001F;
+    while ((CAN0->MCR && CAN_MCR_FRZACK_MASK) >> CAN_MCR_FRZACK_SHIFT)
+    {
+    }
+    while ((CAN0->MCR && CAN_MCR_NOTRDY_MASK) >> CAN_MCR_NOTRDY_SHIFT)
+    {
+    }
 }
