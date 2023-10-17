2a007ca610f928a2540b8556958073769b45274a
lemons <chanhvuive@gmail.com>
2023-10-17 14:12:32 +0700

    ádasdasd

--- Control_PWM_with_buttons/Debug_FLASH/src/NVIC.d
+++
@@ -1,7 +0,0 @@
-src/NVIC.o: ../src/NVIC.c \
- C:/Users/ASUS/Documents/testing_0/Control_PWM_with_buttons/include/NVIC_Type.h \
- C:/Users/ASUS/Documents/testing_0/Control_PWM_with_buttons/include/General.h
-
-C:/Users/ASUS/Documents/testing_0/Control_PWM_with_buttons/include/NVIC_Type.h:
-
-C:/Users/ASUS/Documents/testing_0/Control_PWM_with_buttons/include/General.h:
--- Control_PWM_with_buttons/Debug_RAM/src/SIM.d
+++
@@ -1,7 +0,0 @@
-src/SIM.o: ../src/SIM.c \
- C:/Users/ASUS/Documents/testing_0/Control_PWM_with_buttons/include/SIM_Type.h \
- C:/Users/ASUS/Documents/testing_0/Control_PWM_with_buttons/include/General.h
-
-C:/Users/ASUS/Documents/testing_0/Control_PWM_with_buttons/include/SIM_Type.h:
-
-C:/Users/ASUS/Documents/testing_0/Control_PWM_with_buttons/include/General.h:
